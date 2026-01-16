5fee95d4cd7195630ab9a2e84f94dac2b74318fd
walid404 <walid.max28@gmail.com>
2026-01-16 15:01:18 +0200

    Solve Device Error

--- src/controller/eval_controller.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch.utils.data import DataLoader
-from model.training.trainer import eval_epoch
-from model.training.losses import get_loss
-
-def run_evaluation(model, test_set, batch_size=32, config):
-    loader = DataLoader(test_set, batch_size=batch_size)
-    criterion = get_loss(config["training"]["loss"])
-    return eval_epoch(model, loader, criterion)
