d3e875c933f6b235b138893a267b9af7b9889bcd
walid404 <walid.max28@gmail.com>
2026-01-16 23:20:46 +0200

    Add Image Samples

--- src/controller/inference_controller.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-def predict_age(model, image_tensor):
-    model.eval()
-    with torch.no_grad():
-        pred = model(image_tensor.unsqueeze(0).to(DEVICE))
-    return pred.item()
